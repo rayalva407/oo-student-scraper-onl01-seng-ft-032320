c331ae4c6930ddb99086f77932505650a4d40f5d
Ray Alva <rayalva407@gmail.com>
2020-04-13 20:52:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    students_array.each {|key, value| self.send("#{key}=", value)}
+    attributes_hash_array.each {|key, value| self.send("#{key}=", value)}
   end
 
   def self.all
